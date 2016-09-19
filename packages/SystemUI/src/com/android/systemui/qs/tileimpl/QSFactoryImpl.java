963c9fac9986a1e47ef523954372c26d5eb6b309
stofstik <verstegen.daan@gmail.com>
2016-09-19 10:00:29 +0200

    Added show volume panel tile to QS
    ezio84 adapt for O
    
    @beanstown106 PureNexus Edits:
    *also add missing accesibility strings
    *adapt for marshmallow
    
    ezio84 Adapt for N
    
    Change-Id: Icef4a748967fdd4d0ac59ac47d264d078bf4b8c7

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -50,6 +50,7 @@ import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.ThemeTile;
+import com.android.systemui.qs.tiles.VolumeTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WeatherTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -98,6 +99,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("smartpixels")) return new SmartPixelsTile(mHost);
         else if (tileSpec.equals("soundsearch")) return new SoundSearchTIle(mHost);
         else if (tileSpec.equals("theme")) return new ThemeTile(mHost);
+        else if (tileSpec.equals("volume")) return new VolumeTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
