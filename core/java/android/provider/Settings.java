1f8f85878e642f17496c0376969117a130cd05a9
Simao Gomes Viana <xdevs23@outlook.com>
2016-11-13 17:45:25 +0100

    Introduce new lockscreen visualizer implementation
    Some parts of this commit were made with the help of following commit:
    
    https://github.com/CyanogenMod/android_frameworks_base/commit/6b80bafe65c82a9c32f4558626f3f752500b3823
    
    Even though some parts are very similar to that implementation,
    this is made from scratch.
    
    ------------------------------------------------------------------------
    
    Credits (please let me know if you feel left out here):
    
    UtkarshGupta
    cyanogen
    romanbb
    
    And of course, everyone else who contributed
    to the lockscreen visualizer.
    
    ------------------------------------------------------------------------
    
    Change-Id: Iefc218841459ab289b271625ee08f1cb0dde685c
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4578,6 +4578,16 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_WEATHER_TEMP = "status_bar_show_weather_temp";
 
         /**
+         * Lockscreen Visualizer
+         *
+         * @hide
+         */
+        public static final String SHOW_LOCKSCREEN_VISUALIZER =
+                                "show_lockscreen_visualizer";
+        public static final int SHOW_LOCKSCREEN_VISUALIZER_DEFAULT = 1;
+
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4633,7 +4643,8 @@ public final class Settings {
             LOCK_TO_APP_ENABLED,
             NOTIFICATION_SOUND,
             ACCELEROMETER_ROTATION,
-            SHOW_BATTERY_PERCENT
+            SHOW_BATTERY_PERCENT,
+            SHOW_LOCKSCREEN_VISUALIZER
         };
 
         /**
@@ -4696,6 +4707,7 @@ public final class Settings {
             PUBLIC_SETTINGS.add(HAPTIC_FEEDBACK_ENABLED);
             PUBLIC_SETTINGS.add(SHOW_WEB_SUGGESTIONS);
             PUBLIC_SETTINGS.add(VIBRATE_WHEN_RINGING);
+            PUBLIC_SETTINGS.add(SHOW_LOCKSCREEN_VISUALIZER);
         }
 
         /**
