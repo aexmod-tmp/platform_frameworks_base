2dd73fd4a777e98d8a3a633c1e16e7fdd0255eba
maxwen <max.weninger@gmail.com>
2017-09-20 20:54:41 +0800

    1/2] base: unlink notification volume is back -delete old  unused volume images -add new one for notification volumes maybe some day one  will be able to create a vector drawable for this - I am not :)
    base: add vector for notification volume mute
    
    thanks to Marko Man

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4226,6 +4226,13 @@ public final class Settings {
         public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
