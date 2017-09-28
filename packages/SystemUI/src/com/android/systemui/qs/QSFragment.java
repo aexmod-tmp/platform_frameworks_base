1574051695d899008d9415a094edb9a042862808
maxwen <max.weninger@gmail.com>
2017-09-28 19:44:22 +0200

    FWB: all tile scroller for quickbar settings [1/2]
    allow horizontal scrolling through all tiles also
    in the quickbar
    
    SpiritCroc:
    - Allow fancy animation when scroll position is fine
    - Clip left and right, but not top or bottom
    - Adapt to crDroid
    - Bring it to O
    - Fix scrollview stealing touches from brightness slider
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -30,6 +30,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout.LayoutParams;
+import android.widget.HorizontalScrollView;
 
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
@@ -66,6 +67,7 @@ public class QSFragment extends Fragment implements QS {
 
     // omni additions
     private boolean mSecureExpandDisabled;
+    private HorizontalScrollView mQuickQsPanelScroller;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
@@ -84,8 +86,10 @@ public class QSFragment extends Fragment implements QS {
         mContainer = view.findViewById(id.quick_settings_container);
 
         mQSDetail.setQsPanel(mQSPanel, mHeader, (View) mFooter);
+        mQuickQsPanelScroller =
+                (HorizontalScrollView) mHeader.findViewById(R.id.quick_qs_panel_scroll);
         mQSAnimator = new QSAnimator(this,
-                mHeader.findViewById(R.id.quick_qs_panel), mQSPanel);
+                mHeader.findViewById(R.id.quick_qs_panel), mQSPanel, mQuickQsPanelScroller);
 
         mQSCustomizer = view.findViewById(R.id.qs_customize);
         mQSCustomizer.setQs(this);
@@ -130,6 +134,10 @@ public class QSFragment extends Fragment implements QS {
 
     @Override
     public View getHeader() {
+        return mHeader;
+    }
+
+    public QuickStatusBarHeader getQuickStatusBarHeader() {
         return mHeader;
     }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -207,6 +207,7 @@ import com.android.systemui.qs.QSFragment;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.qs.QSTileHost;
 import com.android.systemui.qs.car.CarQSFragment;
+import com.android.systemui.qs.QuickStatusBarHeader;
 import com.android.systemui.recents.Recents;
 import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.recents.events.EventBus;
@@ -481,6 +482,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     // settings
     private QSPanel mQSPanel;
+    private QuickStatusBarHeader mQuickStatusBarHeader;
 
     // 4G instead of LTE
     private boolean mShow4G;
@@ -1341,6 +1343,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     mQSPanel = ((QSFragment) qs).getQsPanel();
                     mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
                     mKeyguardStatusBar.setQSPanel(mQSPanel);
+                    mQuickStatusBarHeader = ((QSFragment) qs).getQuickStatusBarHeader();
                 }
             });
         }
@@ -5435,6 +5438,9 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     public void onClosingFinished() {
         runPostCollapseRunnables();
+        if (mQuickStatusBarHeader != null) {
+            mQuickStatusBarHeader.onClosingFinished();
+        }
         if (!isPanelFullyCollapsed()) {
             // if we set it not to be focusable when collapsing, we have to undo it when we aborted
             // the closing
@@ -6456,6 +6462,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.FORCE_AMBIENT_FOR_MEDIA),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_QUICKBAR_SCROLL_ENABLED),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -6475,6 +6484,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.FORCE_AMBIENT_FOR_MEDIA))) {
                 setForceAmbient();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_QUICKBAR_SCROLL_ENABLED))) {
+		setQSScroller();
             }
             update();
         }
@@ -6493,9 +6505,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             setQsPanelOptions();
             updateTheme();
             setForceAmbient();
+	    setQSScroller();
         }
     }
-
 
     private void setForceAmbient() {
         mAmbientMediaPlaying = Settings.System.getIntForUser(mContext.getContentResolver(),
@@ -6503,6 +6515,12 @@ public class StatusBar extends SystemUI implements DemoMode,
                 UserHandle.USER_CURRENT);
         if (mAmbientMediaPlaying != 0 && mAmbientIndicationContainer != null) {
             ((AmbientIndicationContainer)mAmbientIndicationContainer).setIndication(mMediaMetadata);
+        }
+    }
+
+    private void setQSScroller() {
+        if (mQuickStatusBarHeader != null) {
+            mQuickStatusBarHeader.updateSettings();
         }
     }
 
