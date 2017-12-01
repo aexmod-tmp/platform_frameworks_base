32e2d9e78bbc3012bee5a5da3972a4406371cd72
DennySPB <dennyspb@gmail.com>
2017-12-01 14:49:58 +0300

    Revert "base: Introduce SubstratumService"
    This reverts commit 7b899c4e0b10ab2e016bbb18ae18a7fac7af3a1a.
    
    Change-Id: Ie738605d47105678412c75e875df1f1a4f17918b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7924,11 +7924,6 @@ public final class Settings {
         public static final String LOCK_POWER_MENU_DISABLED = "lockscreen_power_menu_disabled";
 
         /**
-         * Force authorize Substratum (or equivalent) frontend calling packages by ThemeInterfacer
-         * The value is boolean (1 or 0).
-         * @hide
-         */
-        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
 
         /**
          * The TCP/IP port to run ADB on, or -1 for USB
