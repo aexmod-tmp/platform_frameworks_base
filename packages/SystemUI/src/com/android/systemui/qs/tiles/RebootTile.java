8ea4fb5f630699f7f44e7ecfc2b7dae8d931c3fd
Alex Cruz <du.alexcruz@gmail.com>
2018-02-11 17:13:15 -0500

    Make sure the Reboot tile works under all conditions
    This fixes a fail in where the reboot tile would work great if the keyguard
    was secured but if the user attempted to launch it while it wasn't secured
    it wouldn't work after that.
    
    There was no action for it to perform. Now there is.
    
    Change-Id: Ie0e2f7a21a967e75e497c9d6c81af6749dd4c465

--- packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
@@ -89,6 +89,14 @@ public class RebootTile extends QSTileImpl<BooleanState> {
                         }
                     });
                     return;
+                } else {
+                    try {
+                        if (mRebootToRecovery)
+                            mBarService.advancedReboot(PowerManager.REBOOT_RECOVERY);
+                        else
+                            mBarService.reboot(false);
+                    } catch (RemoteException e) {
+                    }
                 }
             }
         }, 500);
