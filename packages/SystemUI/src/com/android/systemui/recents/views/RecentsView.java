98ff52b3857720fda37d2dac29960daef28a88b1
jar2105 <jara2105@mail.ru>
2018-03-22 19:18:13 +0000

    SystemUI: update RAM bar translations

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -631,7 +631,7 @@ public class RecentsView extends FrameLayout {
         mAm.getMemoryInfo(memInfo);
             int available = (int)(memInfo.availMem / 1048576L);
             int max = (int)(getTotalMemory() / 1048576L);
-            mMemText.setText("Free RAM: " + String.valueOf(available) + "MB");
+            mMemText.setText(getContext().getResources().getString(R.string.recents_free_ram) + " " + String.valueOf(available) + "MB");
             mMemBar.setMax(max);
             mMemBar.setProgress(available);
     }
