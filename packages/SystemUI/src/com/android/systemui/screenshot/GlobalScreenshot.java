a3d80954a9b3b1e609b4a347c134ae047883abeb
Wang Han <wanghan1995315@gmail.com>
2018-05-11 12:47:40 +0200

    SystemUI: Don't append app name to file on lockscreen
    When device is locked, current implementation will append the current
    app name to file when taking screenshot. This causes information leaks.
    
    To prevent this, we check for isKeyGuardLocked() and fall back not to
    append the app name when taking screenshot on lockscreen.
    
    Change-Id: I04498faf51986e1a3723a32befd97d29e1f3fe58

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -28,6 +28,7 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityOptions;
 import android.app.admin.DevicePolicyManager;
+import android.app.KeyguardManager;
 import android.app.Notification;
 import android.app.Notification.BigPictureStyle;
 import android.app.NotificationManager;
@@ -156,8 +157,9 @@ class SaveImageInBackgroundTask extends AsyncTask<Void, Void, Void> {
         mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE, imageDate);
         final PackageManager pm = context.getPackageManager();
         ActivityInfo info = NitrogenUtils.getRunningActivityInfo(context);
-        if (info != null) {
-            CharSequence appName = pm.getApplicationLabel(info.applicationInfo);
+	boolean onKeyguard = context.getSystemService(KeyguardManager.class).isKeyguardLocked();
+        if (!onKeyguard && info != null) {
+        CharSequence appName = pm.getApplicationLabel(info.applicationInfo);
             if (appName != null) {
                 // replace all spaces and special chars with an underscore
                 String appNameString = appName.toString().replaceAll("[\\\\/:*?\"<>|\\s]+", "_");
