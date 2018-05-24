b6be79640bb6ef2e8faa0e5fbbb392aaed8f3421
Alex <alex@dirtyunicorns.com>
2018-05-23 23:14:17 -0500

    Allow to disable the Wallpaper tint [1/2]
    In Oreo, Google introduced an API to easily extract the color of wallpaper.
    This paved the way for a lot of neat things, some of which we took advantage
    of with Pulse. Unfortunately this also meant that Gogole went ahead and applied
    a tint based on the user's wallpaper in a few areas that really should of never
    got a tint. The power menu, recents, keyguard and notification shade all got
    tagged for this.
    
    This commit will allow the user to toggle the wallpaper tint.
    
    Default behavior is still in play so we're not shifting away from the AOSP look
    and feel.
    
    Change-Id: Iae5f0223fe5fb0e8c78e4fc81c907a73916b6a8d
    Signed-off-by: arghyac35 <arghyac35@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -1749,7 +1749,9 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
                     .setUpdateListener(animation -> {
                         int alpha = (int) ((Float) animation.getAnimatedValue()
                                 * ScrimController.GRADIENT_SCRIM_ALPHA * 255);
-                        mGradientDrawable.setAlpha(alpha);
+                        int transparent = (int) ((Float) animation.getAnimatedValue()
+                                * ScrimController.CUSTOM_GRADIENT_SCRIM_ALPHA * 255);
+                        mGradientDrawable.setAlpha(showWallpaperTint(mContext) ? alpha : transparent);
                     })
                     .withEndAction(() -> getWindow().getDecorView().requestAccessibilityFocus())
                     .start();
@@ -1768,7 +1770,9 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
                     .setUpdateListener(animation -> {
                         int alpha = (int) ((1f - (Float) animation.getAnimatedValue())
                                 * ScrimController.GRADIENT_SCRIM_ALPHA * 255);
-                        mGradientDrawable.setAlpha(alpha);
+                        int transparent = (int) ((1f - (Float) animation.getAnimatedValue())
+                                * ScrimController.CUSTOM_GRADIENT_SCRIM_ALPHA * 255);
+                        mGradientDrawable.setAlpha(showWallpaperTint(mContext) ? alpha : transparent);
                     })
                     .start();
         }
@@ -1826,4 +1830,8 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
         }
     }
 
+    private static boolean showWallpaperTint(Context context) {
+        return Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.WALLPAPER_POWER_MENU_TINT, 1, UserHandle.USER_CURRENT) == 1;
+    }
 }
