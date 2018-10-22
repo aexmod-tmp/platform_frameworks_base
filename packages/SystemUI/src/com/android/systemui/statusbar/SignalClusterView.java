7b7851a60e7ddc643718cef3284c95f8b3010d0b
a.derendyaev <a.derendyaev@magdv.com>
2018-10-22 15:57:30 +0700

    Added VOLTE icon
    Signed-off-by: a.derendyaev <a.derendyaev@magdv.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -301,7 +301,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
     @Override
     public void setMobileDataIndicators(IconState statusIcon, IconState qsIcon, int statusType,
             int qsType, boolean activityIn, boolean activityOut, String typeContentDescription,
-            String description, boolean isWide, int subId, boolean roaming) {
+            String description, boolean isWide, int subId, boolean roaming, boolean volte) {
         PhoneState state = getState(subId);
         if (state == null) {
             return;
